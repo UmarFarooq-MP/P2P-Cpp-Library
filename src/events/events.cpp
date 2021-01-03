faa1c85dabde5fc81969ab1c8c33e20fa26a30a6
UmarFarooq-MP <umarfarooq.waheed@gmail.com>
2021-01-04 03:12:06 +0500

    events-added

---
+++ src/events/events.cpp
@@ -0,0 +1,5 @@
+//
+// Created by umar on 4/1/21.
+//
+
+#include "events.h"
---
+++ src/events/events.h
@@ -0,0 +1,19 @@
+//
+// Created by umar on 4/1/21.
+//
+
+#ifndef P2P_CPP_EVENTS_H
+#define P2P_CPP_EVENTS_H
+
+enum EVENT {
+    ONPEERCONNECT,
+    ONPEERDISCONNECT
+};
+
+class Events {
+    
+
+};
+
+
+#endif //P2P_CPP_EVENTS_H
